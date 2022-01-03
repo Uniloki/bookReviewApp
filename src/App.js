82081c7a30578a95ed67cdb03c40c3f075dd6033
Nicholas Wooden <uniloki009@gmail.com>
2022-01-02 22:53:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [bookName, setBookName] = useState('')
+	const [bookReview, setBookReview] = useState('')
+	const [bookReviewList, setBookReviewList] = useState([])
+	const [newBookReview, setNewBookReview] = useState('')
+
+	useEffect(() => {
+		Axios.get('http://cruddatabase-book.herokuapp.com/api/get').then((res) =>
+			setBookReviewList(res.data)
+		)
+	})
+	const submitReview = () => {
+		Axios.post('http://cruddatabase-book.herokuapp.com/api/insert', {
+			bookName: bookName,
+			bookReview: bookReview,
+		}).then(() => {
+			setBookReviewList([
+				...bookReviewList,
+				{ bookName: bookName, bookReview: bookReview },
+			])
+		})
+	}
+	const deleteReview = (book) => {
+		Axios.delete(`http://cruddatabase-book.herokuapp.com/api/delete/${book}`)
+	}
+	const updateReview = (book) => {
+		Axios.put(`http://cruddatabase-book.herokuapp.com/api/update`, {
+			bookName: book,
+			bookReview: newBookReview,
+		})
+		setNewBookReview('')
+	}
+
+	return (
+		<div className="App">
+			<h1> Book Reviews! </h1>
+			<div className="form">
+				<label> Book Name: </label>
+				<input
+					type="text"
+					name="bookName"
+					onChange={(e) => {
+						setBookName(e.target.value)
+					}}
+				></input>
+				<label> Book Review: </label>
+				<input
+					type="text"
+					name="review"
+					onChange={(e) => {
+						setBookReview(e.target.value)
+					}}
+				></input>
+
+				<button onClick={submitReview}> Sumbit </button>
+			</div>
+
+			<div>
+				{bookReviewList.map((book) => {
+					return (
+						<div>
+							<h1> {book.bookName} </h1>
+							<p> {book.bookReview} </p>
+							<button
+								onClick={() => {
+									deleteReview(book.bookName)
+								}}
+							>
+								{' '}
+								Delete{' '}
+							</button>
+							<input
+								type="text"
+								name="edit"
+								onChange={(e) => {
+									setNewBookReview(e.target.value)
+								}}
+							/>
+							<button
+								onClick={() => {
+									updateReview(book.bookName)
+								}}
+							>
+								{' '}
+								Update{' '}
+							</button>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
